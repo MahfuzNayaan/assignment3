1fb0831027837c2949b3c6daf6e19e824122d8ee
MahfuzNayaan <mahfuzali0123@gmail.com>
2025-08-23 00:07:34 +0600

    temparature code

--- temperature.py
+++ temperature.py
@@ -1,1 +1,7 @@
-temperature=int(input("Enter temparature"))
+temperature=int(input("Enter temparature: "))
+if temperature <15:
+    print("Cold")
+elif temperature>=15 and temperature<30:
+    print("Warm")
+else:
+    print("Hot")
