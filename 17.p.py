21a10c80a701b3b92e811b21f654332a271dcb42
MahfuzNayaan <mahfuzali0123@gmail.com>
2025-08-23 15:02:08 +0600

    y

---
+++ 17.p.py
@@ -0,0 +1,5 @@
+learning_rate=float(input("Enter learning rate as 0.1: "))
+while learning_rate>0.001:
+    print("learningb rate: ",learning_rate)
+    learning_rate=learning_rate /2
+print("Final learning  rate: ",learning_rate)
