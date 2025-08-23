8b1515de41fb8c1f64b78538ad7130270e2b6e4c
MahfuzNayaan <mahfuzali0123@gmail.com>
2025-08-23 15:40:38 +0600

    bro

---
+++ 18.py
@@ -0,0 +1,9 @@
+num=int(input("Enter a number: "))
+count=0
+while count<=num:
+    if num%2==0:
+      print("NUmber of even occur",count)
+      count=count+1
+    else:
+     pass
+print("Final count",count)
